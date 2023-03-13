f47aeb4637f1e9649137cbf20b04da8c9b90e3b1
Tyrell Daniel Nelson <ty.dan.nel@gmail.com>
2023-03-13 11:39:49 +0200

    Some more refactoring

--- store.js
+++ store.js
@@ -10,26 +10,30 @@ export default class extends Map {
     toArray() {
         return Array.from(this.entries());
     }
+    toJson() {
+        return JSON.stringify(this.toArray());
+    }
     fromArray(array) {
-        for (const [key, value] of array) {
-            this.set(key, value);
-        }
+        return array.forEach(([key, value]) => {
+            return this.set(key, value);
+        });
     }
     hasValue(value) {
         return Array.from(this.values()).find(v => v === value);
     }
     set(key, value) {
         // No duplicate urls or keys
-        if (!this.hasValue(value) && !this.has(key)) {
-            super.set(key, value);
+        if (this.hasValue(value) || this.has(key)) {
+            throw new Error('Key and value should be unique,');
         }
+        return super.set(key, value);
     }
     async save() {
-        await Fs.writeFile(this.#path, JSON.stringify(this.toArray()));
+        return await Fs.writeFile(this.#path, this.toJson());
     }
     async load() {
         const rawData = await Fs.readFile(this.#path);
         const data = JSON.parse(rawData.toString());
-        this.fromArray(data);
+        return this.fromArray(data);
     }
 }
